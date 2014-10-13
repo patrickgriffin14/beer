6c99c96ebdb51d1786cae7f82f707c2842a52d5c
Patrick Griffin <p2dag2@gmail.com>
2014-10-13 20:12:24 +0100

    added some unit tests

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -1,6 +1,6 @@
 class Brew < ActiveRecord::Base
 validates :title, :description, :image_url, :website, :origin, presence: true
-validates :title, uniqueness: true
+validates :title, :website, uniqueness: true
 validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 validates :image_url, allow_blank: true, format: {
   with: %r{\.(gif|jpg|png|jpeg)\Z}i,
