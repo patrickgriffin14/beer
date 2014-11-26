7432c41becf3e88a0c4fef9a0a66dc7f25d9df69
patrickgriffin14 <p.griffin6@nuigalway.ie>
2014-11-26 16:28:25 +0000

    added quantities to line items table

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,3 +1,12 @@
 class Favourite < ActiveRecord::Base
 has_many :line_items, dependent: :destroy
+def add_brew(brew_id)
+  current_item = line_items.find_by(brew_id: brew_id)
+  if current_item
+    current_item.quantity += 1
+  else
+    current_item = line_items.build(brew_id: brew_id)
+  end
+  current_item
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126101941) do
+ActiveRecord::Schema.define(version: 20141126161031) do
 
   create_table "brews", force: true do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20141126101941) do
     t.integer  "favourite_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",     default: 1
   end
 
   add_index "line_items", ["brew_id"], name: "index_line_items_on_brew_id"
